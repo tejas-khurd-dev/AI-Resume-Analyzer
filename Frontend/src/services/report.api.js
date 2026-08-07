ed8f9a78708921980eba7cd52e7503ef5d3f53d0
tejas-khurd-dev <tejaskhurd.dev@gmail.com>
2026-08-07 16:13:56 +0530

    chore: update API base URLs for production

--- Frontend/src/services/report.api.js
+++ Frontend/src/services/report.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000/api/ai-report",
+    baseURL: `${import.meta.env.VITE_API_URL}/ai-report`,
     withCredentials:true
 })
 
